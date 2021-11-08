d2194529d42e650c49b1477c8cc0ade157b67f97
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2021-11-09 00:23:27 +0700

    add operators folder

---
+++ List/list.py
@@ -0,0 +1,13 @@
+"""
+Learn List
+- List bisa duplicate
+- Bisa diubah isinya 
+
+"""
+
+data = ["apple", "banana"]
+beli = ("semangka", "melon")
+print(data)
+
+data.extend(beli)
+print(data)
---
+++ Operators/operators.py
@@ -0,0 +1,7 @@
+"""
+Python Assignment Operators
+
+"""
+
+# '='
+x = 5
